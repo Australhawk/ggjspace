6bbb0c19e983c18b4a31a72d510a2a0e0060df07
Sebastián Paiva <australhawk@gmail.com>
2018-01-26 23:01:16 -0300

    Added Level, Scripts & ScriptableObjects

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+	// Import ScriptableObject Data
+	public PlanetObject planetObject;
+	// Use this for initialization
+	void Start () {
+
+		// Set RigidbodyMass
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		if (rigidbody != null) {
+			rigidbody.mass = this.planetObject.planetMass;
+		}
+		// Set Planet Radius
+		if (this.planetObject.planetRadius != null) {
+			float diameter = this.planetObject.planetRadius * 2;
+			this.transform.localScale = new Vector3 (diameter, diameter, diameter);	
+		}
+		// Set Planet Material
+		if (this.planetObject.material != null) {
+			GetComponent<Renderer> ().material = this.planetObject.material;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlanetObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlanetData", menuName = "Planet", order = 1)]
+public class PlanetObject : ScriptableObject {
+	public string planetName;
+	public float planetRadius;
+	public Material material;
+	public float planetMass;
+}
