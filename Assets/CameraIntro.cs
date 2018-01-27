543e12b67a31ad7a3274bf470d625f508888167a
Sebastián Paiva <australhawk@gmail.com>
2018-01-27 15:00:43 -0300

    Added Intro

---
+++ Assets/CameraIntro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraIntro : MonoBehaviour {
+
+	public Transform followTarget;
+	public Vector3 offset = new Vector3(10f,0,0);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = followTarget.position + offset;
+	}
+}
---
+++ Assets/Scripts/AsteroidIntro.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidIntro : MonoBehaviour {
+
+	public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = new Vector3 (1, -1, 0) * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
