3e18ebad613b136f542e3fa4dfdfe4cb9a181e3c
Gabriel <gabriel.chfernandez@gmail.com>
2018-01-27 18:27:00 -0300

    Particles Terroso - Continuous Music

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Planet : MonoBehaviour {
 
     // Import ScriptableObject Data
+    public Vector3 initialPos;
     public PlanetObject planetObject;
     private bool finished;
     private Vector3 velocity;
-
+    
     // Use this for initialization
     void Start() {
+        initialPos = transform.position;
         finished = false;
         // Set RigidbodyMass
         Rigidbody rigidbody = GetComponent<Rigidbody>();
