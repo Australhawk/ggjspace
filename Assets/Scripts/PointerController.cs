627558b83a3884c5eb64cec993834eef0fc41c1b
tatanpoker09 <christian@eilers.cl>
2018-01-28 08:31:32 -0300

    Pointer Arrow

--- Assets/Scripts/PointerController.cs
+++ Assets/Scripts/PointerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,7 +14,19 @@ public class PointerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        UpdatePlanet();
         this.transform.LookAt(currentRing.transform);
         this.transform.position = currentPlanet.transform.position+Vector3.up*3;
 	}
+
+    private void UpdatePlanet() {
+        currentPlanet = GameManager.instance.currentPlanet.GetComponent<Planet>();
+        List<RingController> rings = GameManager.instance.GetComponent<RingManager>().rings;
+        foreach(RingController ring in rings) {
+            if (ring.planet.Equals(currentPlanet)) {
+                currentRing = ring;
+                break;
+            }
+        }
+    }
 }
